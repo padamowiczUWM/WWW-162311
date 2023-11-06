777ba3eb0ddb7426443e225fbc94d6f07a55ff9d
Patryk Adamowicz <patrykadam.dev@gmail.com>
2023-11-06 13:25:39 +0100

    work

--- issue/lab_views.py
+++ issue/lab_views.py
@@ -1,113 +1,86 @@
+from django.http import Http404
 from django.shortcuts import render
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Person, Team
 from .serializers import PersonSerializer, TeamSerializer
 
 
-# określamy dostępne metody żądania dla tego endpointu
-@api_view(['GET', 'POST'])
-def person_list(request):
-    """
-    Lista wszystkich obiektów modelu Person.
-    """
-    if request.method == 'GET':
+class PersonList(APIView):
+    def get(self, request):
         name = request.GET.get('name', '')
 
         persons = Person.objects.filter(name__icontains=name)
         serializer = PersonSerializer(persons, many=True)
         return Response(serializer.data)
 
-    if request.method == 'POST':
+    def post(self, request):
         serializer = PersonSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class PersonDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Person.objects.get(pk=pk)
+        except Person.DoesNotExist:
+            raise Http404
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def person_detail(request, pk):
-
-    """
-    :param request: obiekt DRF Request
-    :param pk: id obiektu Person
-    :return: Response (with status and/or object/s data)
-    """
-    try:
-        person = Person.objects.get(pk=pk)
-    except Person.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    """
-    Zwraca pojedynczy obiekt typu Person.
-    """
-    if request.method == 'GET':
+    def get(self, request, pk):
         person = Person.objects.get(pk=pk)
         serializer = PersonSerializer(person)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        serializer = PersonSerializer(person, data=request.data)
+    def put(self, request, pk):
+        serializer = PersonSerializer(self.get_object(pk), data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
-        person.delete()
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-@api_view(['GET'])
-def team_list(request):
-    """
-    Lista wszystkich obiektów modelu Team.
-    """
-    if request.method == 'GET':
+class TeamList(APIView):
+    def get(self, request):
         teams = Team.objects.all()
         serializer = TeamSerializer(teams, many=True)
         return Response(serializer.data)
 
-
-    if request.method == 'POST':
+    def post(self, request):
         serializer = TeamSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class TeamDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Team.objects.get(pk=pk)
+        except Team.DoesNotExist:
+            raise Http404
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def team_detail(request, pk):
-
-    """
-    :param request: obiekt DRF Request
-    :param pk: id obiektu Team
-    :return: Response (with status and/or object/s data)
-    """
-    try:
-        team = Team.objects.get(pk=pk)
-    except Team.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    """
-    Zwraca pojedynczy obiekt typu Team.
-    """
-    if request.method == 'GET':
+    def get(self, request, pk):
         team = Team.objects.get(pk=pk)
         serializer = TeamSerializer(team)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        serializer = TeamSerializer(team, data=request.data)
+    def put(self, request, pk):
+        serializer = TeamSerializer(self.get_object(pk), data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
-        team.delete()
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
--- issue/urls.py
+++ issue/urls.py
@@ -8,10 +8,10 @@ issue_router = routers.DefaultRouter()
 issue_router.register('issue', IssueViewSet, basename='issue')
 
 urlpatterns = [
-    path('persons/', lab_views.person_list),
-    path('persons/<int:pk>/', lab_views.person_detail),
-    path('teams/', lab_views.team_list),
-    path('teams/<int:pk>/', lab_views.team_detail),
+    path('persons/', lab_views.PersonList.as_view()),
+    path('persons/<int:pk>/', lab_views.PersonDetail.as_view()),
+    path('teams/', lab_views.TeamList.as_view()),
+    path('teams/<int:pk>/', lab_views.TeamDetail.as_view()),
 ]
 
 urlpatterns += issue_router.urls
